62b63beeddfa03b0c8ff1f595efc961a0412feed
Saswat <mohantysaswat154@gmail.com>
2026-01-30 09:27:03 +0530

    using client components efficiently

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -17,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies community</Link>
+              <NavLink href="/community">Foodies community</NavLink>
             </li>
           </ul>
         </nav>
